e6be27cde68b47267328648ba2d2c6d42e3d5cb3
ChaoticWagon <69493011+ChaoticWagon@users.noreply.github.com>
2021-11-14 23:15:54 -0500

    Update Items.kt

--- src/main/kotlin/io/github/TeamRickRoll/Items.kt
+++ src/main/kotlin/io/github/TeamRickRoll/Items.kt
@@ -2,121 +2,123 @@ package io.github.TeamRickRoll
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.item.ItemMetaBuilder
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
 
 class Items {
 
-    var cheese = ItemStack.builder(Material.APPLE)
+    val cheese = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Cheese", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000003)
-            })
+            }
             .build()
 
-    var lettuce = ItemStack.builder(Material.APPLE)
+    val lettuce = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Lettuce", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
-                it.customModelData(1000006)
-            })
+            .meta {
+             it.customModelData(1000006) }
             .build()
 
-    var butter = ItemStack.builder(Material.APPLE)
+    val butter = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Butter", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000001)
-            })
+            }
             .build()
 
-    var cheeseSlice = ItemStack.builder(Material.APPLE)
+    val cheeseSlice = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Cheese Slice", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000002)
-            })
+            }
             .build()
 
-    var cookedTurkey = ItemStack.builder(Material.APPLE)
+    val cookedTurkey = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Cooked Turkey", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000004)
-            })
+            }
             .build()
 
-    var lettuceLeaf = ItemStack.builder(Material.APPLE)
+    val lettuceLeaf = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Lettuce Leaf", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000005)
-            })
+            }
             .build()
 
-    var onion = ItemStack.builder(Material.APPLE)
+    val onion = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Onion", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000007)
-            })
+            }
             .build()
 
-    var potatoSlice = ItemStack.builder(Material.APPLE)
+    val potatoSlice = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Potato Slice", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000008)
-            })
+            }
             .build()
 
-    var rawTurkey = ItemStack.builder(Material.APPLE)
+    val rawTurkey = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Raw Turkey", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000009)
-            })
+            }
             .build()
 
-    var toast = ItemStack.builder(Material.APPLE)
+    val toast = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Toast", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000010)
-            })
+            }
             .build()
 
-    var tomatoSlice = ItemStack.builder(Material.APPLE)
+    val tomatoSlice = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Tomato Slice", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000011)
-            })
+            }
             .build()
 
-    var tomato = ItemStack.builder(Material.APPLE)
+    val tomato = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Tomato", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000012)
-            })
+            }
             .build()
 
-    var turkeySalad = ItemStack.builder(Material.APPLE)
+    val turkeySalad = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Turkey Salad", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000013)
-            })
+            }
             .build()
 
-    var turkeySandwich = ItemStack.builder(Material.APPLE)
+    val turkeySandwich = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Turkey Sandwich", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000014)
-            })
+            }
             .build()
 
-    var turkeyBurger = ItemStack.builder(Material.APPLE)
+    val turkeyBurger = ItemStack.builder(Material.APPLE)
             .displayName(Component.text("Turkey Burger", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1000015)
-            })
+            }
             .build()
 
-    var knife = ItemStack.builder(Material.IRON_SWORD)
+    val knife = ItemStack.builder(Material.IRON_SWORD)
             .displayName(Component.text("Knife", NamedTextColor.WHITE))
-            .meta(metaBuilder = {
+            .meta {
                 it.customModelData(1234567)
-            })
+            }
             .build()
+
+
 
 }
